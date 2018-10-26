41351ef1640bfa0901db4e31d0b70ad6f6c8b334
patinbsb <patinbsb@hotmail.com>
2018-10-26 13:09:55 +0100

    setup front page to allow passing of VOD link

--- TwitchFrontend/src/app/app.component.ts
+++ TwitchFrontend/src/app/app.component.ts
@@ -8,4 +8,10 @@ import {MatButtonModule} from '@angular/material';
 })
 export class AppComponent {
   title = 'Twitch Highlight Reel Generator';
+
+  onSubmit() {
+    // Here we do the vod download and all the other stuff, keeping the user informed of steps
+    // Then we direct to new page with outcome, some stats, and eiother highlight vid download or embedded video.
+    console.log('success');
+  }
 }
