a1ac831f7bb266cbe72fc193a504ae42d958f59c
patinbsb <patinbsb@hotmail.com>
2019-02-21 16:47:35 +0000

    Kill detect count complete for both hightlights and matches. Working next on accuracy improvements

--- Scripts/Successful_match/Clip_Analyzer.py
+++ Scripts/Successful_match/Clip_Analyzer.py
@@ -11,7 +11,7 @@ It then will create a representation of kills over time
 '''
 
 
-def capture_kill_differences(cap):
+def capture_kill_differences(cap, display_matched_frames=False):
     kills_over_time = []
     # Collecting video metadata for exporting to video
     fps = cap.get(cv2.CAP_PROP_FPS)
@@ -19,51 +19,58 @@ def capture_kill_differences(cap):
     number_of_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
     prev_red_score = None
+    current_frame = 1
     # Iterate over each frame
     while cap.isOpened():
         ret, frame = cap.read()
         if ret is True:
+            # Process frame to make score white and background black
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            #frame = cv2.threshold(frame, 10, 255, cv2.THRESH_TOZERO)[1]
+            frame = cv2.threshold(frame, 50, 255, cv2.THRESH_TOZERO)[1]
+
+            # Get region of blue teams score and red teams score
             blue_score = frame[2:30, int(video_width/2 - 25): int(video_width/2 - 1)]*2
             red_score = frame[2:30, int(video_width/2 + 8): int(video_width/2 + 25)]*2
+            # Used to filter out scene transition effects that give false positives
+            control_icon = frame[2:30, int(video_width/2 - 6): int(video_width/2 + 10)]*2
 
-            # TODO capture sword icon and check if within tolerance to filter out issues
-            # TODO with false positives from scene transition effects
             # Starting condition
             if prev_red_score is None:
                 prev_red_score = red_score
                 prev_blue_score = blue_score
+                prev_control_icon = control_icon
             else:
+                # Get differences between the previous frame and current frame
                 (diff_score_blue, diff_blue) = compare_ssim(prev_blue_score, blue_score, full=True)
                 (diff_score_red, diff_red) = compare_ssim(prev_blue_score, blue_score, full=True)
+                (diff_control_icon, diff_control) = compare_ssim(prev_control_icon, control_icon, full=True)
 
-                if diff_score_blue > 0.90:
-                    # cv2.imshow('curr same', blue_score)
-                    # cv2.imshow('prev same', prev_blue_score)
-                    # if cv2.waitKey(0) & 0xFF == ord('q'):
-                    #     break
-                    print ('similar')
-                else:
-                    print('different')
-                    cv2.imshow('curr different', blue_score)
-                    cv2.imshow('prev different', prev_blue_score)
-                    if cv2.waitKey(0) & 0xFF == ord('q'):
-                        prev_red_score = red_score
-                        prev_blue_score = blue_score
-                        continue
+                # Check frame differences are below a certain threshold and the control is unchanged
+                if diff_score_blue < 0.95 and diff_control_icon > 0.80:
+                    # kill change registered on this frame
+                    kills_over_time.append(current_frame)
+                    if display_matched_frames is True:
+                        cv2.imshow('curr different', blue_score)
+                        cv2.imshow('prev different', prev_blue_score)
+                        if cv2.waitKey(0) & 0xFF == ord('q'):
+                            prev_red_score = red_score
+                            prev_blue_score = blue_score
+                            continue
+
+                if diff_score_red < 0.95 and diff_control_icon > 0.80:
+                    # kill change registered on this frame
+                    kills_over_time.append(current_frame)
+                    if display_matched_frames is True:
+                        cv2.imshow('curr different', red_score)
+                        cv2.imshow('prev different', prev_red_score)
+                        if cv2.waitKey(0) & 0xFF == ord('q'):
+                            prev_red_score = red_score
+                            prev_blue_score = blue_score
+                            continue
                 prev_red_score = red_score
                 prev_blue_score = blue_score
-
-
-
-            # cv2.imshow('left', cropped_frame_left)
-            # if cv2.waitKey(0) & 0xFF == ord('q'):
-            #     break
-            # cv2.imshow('right', cropped_frame_right)
-            # if cv2.waitKey(0) & 0xFF == ord('q'):
-            #     break
-
+                prev_control_icon = control_icon
+                current_frame += 1
         # No more frames to process
         if ret is False:
             cap.release()
@@ -106,12 +113,32 @@ for clip in clips:
     if 'match' in clip and '.mp4' in clip:
         matches.append(clip)
 
+# Process number of kills in each highlight
+highlight_kill_frames = []
 for highlight in highlights:
     video_to_process = current_directory_path + '\\' + highlight
     # Load the video into cv2
     cap = cv2.VideoCapture(video_to_process)
-    print ('processing ' + highlight)
-    capture_kill_differences(cap)
+    print('processing ' + highlight)
+    kill_frames = capture_kill_differences(cap)
+    highlight_kill_frames.append((highlight, kill_frames))
 
+for highlight_name in highlight_kill_frames:
+    print(highlight_name[0])
+    print(len(highlight_name[1]))
+
+# Process number of kills in each match
+match_kill_frames = []
+for match in matches:
+    video_to_process = current_directory_path + '\\' + match
+    # Load the video into cv2
+    cap = cv2.VideoCapture(video_to_process)
+    print('processing ' + match)
+    kill_frames = capture_kill_differences(cap)
+    match_kill_frames.append((match, kill_frames))
+
+for match_name in match_kill_frames:
+    print(match_name[0])
+    print(len(match_name[1]))
 
 
